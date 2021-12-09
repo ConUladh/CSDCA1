84c7165b637ddb7de5b39d04de1e9ee26ca9a8d1
Stephen McNally <stephen@conuladh.com>
2021-12-09 15:40:43 +0000

    Added K6 test script

---
+++ k6test.js
@@ -0,0 +1,27 @@
+import { sleep } from "k6";
+import http from "k6/http";
+
+export const options = {
+  ext: {
+    loadimpact: {
+      distribution: {
+        "amazon:ie:dublin": { loadZone: "amazon:ie:dublin", percent: 100 },
+      },
+    },
+  },
+  stages: [
+    { target: 20, duration: "1m" },
+    { target: 20, duration: "3m30s" },
+    { target: 0, duration: "1m" },
+  ],
+  thresholds: { http_req_duration: ["p(95)<200"] },
+};
+
+export default function main() {
+  let response;
+
+  response = http.get("https://bpcalculator20211123213520.azurewebsites.net");
+
+  // Automatically added sleep
+  sleep(1);
+}
